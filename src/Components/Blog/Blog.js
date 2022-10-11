48c7fe294ed89fd030646c81626f6a2504007ffe
Iftekher-CSE <iftekher.cse@gmail.com>
2022-10-11 20:42:29 +0600

    correct answer show toast added

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -3,7 +3,16 @@ import React from "react";
 const Blog = () => {
     return (
         <div>
-            <h4>Blogs</h4>
+            <h4>
+                Blogs
+                <div className="toast">
+                    <div className="alert alert-info">
+                        <div>
+                            <span>New message arrived.</span>
+                        </div>
+                    </div>
+                </div>
+            </h4>
         </div>
     );
 };
--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,16 +1,29 @@
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/solid";
 import React from "react";
 import Options from "../Options/Options";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Question = ({ quiz }) => {
     const { correctAnswer, id, options, question } = quiz;
-    // console.log(quiz);
+    console.log(quiz);
+    // const showCorrectAnswer = () => toast({ correctAnswer });
+    const showAnswer = () => toast(`Correct answer is: "${correctAnswer}"`);
     return (
-        <div className="p-4 border-2 m-4 rounded-2xl border-orange-600">
-            <h4>Question: {question}</h4>
+        <div className="p-2 border-2 m-3 rounded-2xl border-orange-600">
+            <div className="flex">
+                <h4>Question: {question}</h4>
+                <div className="flex items-center">
+                    <button onClick={showAnswer}>
+                        <QuestionMarkCircleIcon className="h-7 w-7 text-orange-600"></QuestionMarkCircleIcon>
+                    </button>
+                    <ToastContainer />
+                </div>
+            </div>
 
             <div className="pt-4">
                 {options.map(option => (
-                    <Options option={option}></Options>
+                    <Options key={id} option={option}></Options>
                 ))}
             </div>
         </div>
