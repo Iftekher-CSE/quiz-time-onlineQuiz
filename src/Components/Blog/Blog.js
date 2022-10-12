34d665c99d996b9ec004d5756038f4cd2e7eac65
Iftekher-CSE <iftekher.cse@gmail.com>
2022-10-12 18:14:39 +0600

    all three question answered

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -8,43 +8,74 @@ const Blog = () => {
             </h4>
             <div
                 tabIndex={0}
-                className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
+                className="collapse collapse-plus border-2 border-orange-500 bg-base-100 rounded-box px-5"
             >
                 <div className="collapse-title text-xl font-medium">
                     What is the purpose of using react-router?
                 </div>
                 <div className="collapse-content">
                     <p>
-                        tabIndex={0} attribute is necessary to make the div
-                        focusable
+                        <strong>Routing</strong> is a process in which a user is
+                        directed to different pages based on their action or
+                        request. <strong>React Router</strong> is a
+                        fully-featured client and server-side routing library
+                        for React for building user interfaces and routing among
+                        them. React Router, and dynamic API, client-side
+                        routing, allows developers to build a
+                        <strong> single-page</strong> web application with
+                        navigation <strong>without the page refreshing </strong>
+                        as the user navigates. Without React Router, it is not
+                        possible to display multiple views in React
+                        applications.
                     </p>
                 </div>
             </div>
             <div
-                tabIndex={0}
-                className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
+                tabIndex={1}
+                className="collapse collapse-plus border-2 border-orange-500 bg-base-100 rounded-box px-5"
             >
                 <div className="collapse-title text-xl font-medium">
                     How does Context API works?
                 </div>
                 <div className="collapse-content">
                     <p>
-                        tabIndex={0} attribute is necessary to make the div
-                        focusable
+                        The <strong>React Context API</strong> is a way for a
+                        React app to effectively produce global variables that
+                        can be passed around. This is the alternative to{" "}
+                        <strong>"prop drilling"</strong> or moving props from
+                        grandparent to child to parent, and so on. <br />
+                        <strong>React Context API</strong> creates a global
+                        variables and a provider by using{" "}
+                        <strong>createContext</strong> function. Provider is a
+                        component as it's given names and this provider is used
+                        to select which child can access to the global variable
+                        created by context. We can pass any kinds of{" "}
+                        <strong>value </strong>
+                        within provider. Finally all selected components can
+                        access the global variable with the given value by using{" "}
+                        <strong>useContext</strong> function.
                     </p>
                 </div>
             </div>
             <div
-                tabIndex={0}
-                className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
+                tabIndex={2}
+                className="collapse collapse-plus border-2 border-orange-500 bg-base-100 rounded-box px-5"
             >
                 <div className="collapse-title text-xl font-medium">
                     What is React 'useRef' Hook?
                 </div>
                 <div className="collapse-content">
                     <p>
-                        tabIndex={0} attribute is necessary to make the div
-                        focusable
+                        The <strong>useRef</strong> Hook is a function that
+                        returns a <strong>mutable</strong> ref object whose
+                        <strong>.current</strong> property is initialized with
+                        the passed argument <strong>(initialValue)</strong>. The
+                        returned object will persist for the full lifetime of
+                        the component. <br />
+                        There are two main uses of useRef. one is
+                        <u> Accessing the DOM nodes or React elements</u> and
+                        another is
+                        <u> keeping a mutable variable</u>.
                     </p>
                 </div>
             </div>
--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -12,7 +12,7 @@ const Quiz = () => {
     return (
         <div>
             <h4 className="text-center text-3xl font-bold mt-6 text-orange-600">
-                Quiz On: {loadQuiz.data.name} (Total Pint: {correctCount})
+                Quiz On: {loadQuiz.data.name} (Total Marks: {correctCount})
             </h4>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-6">
                 {loadQuiz.data.questions.map(quiz => (
